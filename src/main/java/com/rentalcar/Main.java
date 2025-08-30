15dbbc560288c65223557823f9fdf82c37575d9f
ardizefi <ardizefi296@gmail.com>
2025-08-30 10:18:09 +0200

    CarRental

--- src/main/java/com/rentalcar/Main.java
+++ src/main/java/com/rentalcar/Main.java
@@ -1,5 +1,7 @@
 package com.rentalcar;
 
+import com.rentalcar.entity.Vehicle;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
